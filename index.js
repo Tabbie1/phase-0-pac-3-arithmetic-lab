c479248d25b11effc5a346ce4fb2f3d14144a591
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-10 17:06:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(){
+
+}
+function subtract(){
+    
+}
+
+function multiply(){
+    
+}
+
+function divide(){
+    
+}
+function add(a=1000,b=1){
+    return `${a+b}`;
+}
+function subtract(a=490,b=2){
+    return `${a-b}`;
+}
+
+function multiply(a=387491,b=1){
+    return `${a*b}`;
+}
+
+
+function divide(a=1.0436005625879043,b=1){
+    return `${a/b}`;
+}
+
+
+function increment(a=419){
+
+    return `${a+1}`;
+}
+function decrement(a=419){
+
+    return `${a-1}`;
+}
+function makeInt(){
+  
+}
+
+function makeInt(n=573){
+  return `${n}`;
+}
+
+function preserveDecimal(n){
+    return parseFloat("2.222");
+}
